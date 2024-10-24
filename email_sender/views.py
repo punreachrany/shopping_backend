8b55df233d5b41f91c6a866df2f90ba739f3e476
Punreach Rany <punreach.rany@yahoo.com>
2024-10-24 17:32:11 +0900

    [chore]: added more detail to the email

--- email_sender/views.py
+++ email_sender/views.py
@@ -1,24 +1,39 @@
-from django.shortcuts import render
 from django.core.mail import send_mail
-from django.http import JsonResponse
-from django.views import View
 from django.conf import settings
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
+from users.authentication import JWTAuthentication
 
 class SendEmailView(APIView):
+    permission_classes = [IsAuthenticated]  # Ensure the user is authenticated
+    authentication_classes = [JWTAuthentication]
+
     def post(self, request):
-        subject = request.data.get('subject', 'No Subject')  # Use request.data
-        message = request.data.get('message', 'No Message')
-        recipient_list = request.data.get('recipient_list', [])  # Expecting a list
+        user = request.user  # Get the logged-in user
+        title = request.data.get('title', 'No Title Provided')
+        message = request.data.get('message', 'No message provided.')
+
+        # Prepare the email body
+        email_body = f"""
+        User Info:
+        Name: {user.name}
+        Email: {user.email}
+        ID: {user.id}
+
+        Message:
+        {message}
+        """
+
+        subject = f'Shopping Inquiry from {user.email} - {title}'
 
         try:
             send_mail(
                 subject,
-                message,
+                email_body,
                 settings.EMAIL_HOST_USER,
-                recipient_list,
+                [settings.EMAIL_HOST_USER],
                 fail_silently=False,
             )
             return Response({'status': 'success', 'message': 'Email sent successfully!'})
