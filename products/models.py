3c895e0e6febe0063310ccafa5c2c0d5744c4fc1
Punreach Rany <punreach.rany@yahoo.com>
2024-10-28 16:01:28 +0900

    [refactor]: changed the model

--- products/models.py
+++ products/models.py
@@ -1,11 +1,20 @@
-# products/models.py
 from django.db import models
 
 class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('Foods', 'Foods'),
+        ('Clothes', 'Clothes'),
+        ('Electronics', 'Electronics'),
+        ('Other', 'Other'),
+    ]
+
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     quantity = models.IntegerField()
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)  # New field
+    out_of_stock = models.BooleanField(default=False)  # New field
+    image_url = models.URLField(max_length=300, blank=True, null=True)  # New field
 
     def __str__(self):
         return self.name
--- products/serializers.py
+++ products/serializers.py
@@ -1,9 +1,9 @@
-# products/serializers.py
 from rest_framework import serializers
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
-    price = serializers.FloatField(read_only=True)
+    price = serializers.FloatField(read_only=True)  # Read-only price field
+
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = '__all__'  # Includes all model fields
