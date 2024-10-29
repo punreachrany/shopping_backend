dd075c0185798f750589b61b692c88d1e6ad5c3a
Punreach Rany <punreach.rany@yahoo.com>
2024-10-29 20:34:10 +0900

    [refactor]: new app

---
+++ loginapp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def logout(request):
+    return render(request, 'index.html')
