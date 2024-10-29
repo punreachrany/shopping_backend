17ba6db901983fd3dd173b9c5adf01f7d42123aa
Punreach Rany <punreach.rany@yahoo.com>
2024-10-29 23:02:49 +0900

    [fix]: auth fixed

--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('register', RegisterView.as_view()),
-    path('login', LoginView.as_view()),
-    path('info', UserView.as_view()),
-    path('logout', LogoutView.as_view()),
-    path('is_logged_in', IsLoggedInView.as_view()),
+    path('register/', RegisterView.as_view()),
+    path('login/', LoginView.as_view()),
+    path('info/', UserView.as_view()),
+    path('logout/', LogoutView.as_view()),
+    path('is_logged_in/', IsLoggedInView.as_view()),
 ]
