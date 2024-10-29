e5bcc5ef6557bc2de6b8d18a0a57433ff626aaeb
Punreach Rany <punreach.rany@yahoo.com>
2024-10-29 20:19:22 +0900

    [refactor]: remove all codes

--- products/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Product
-
-class ProductSerializer(serializers.ModelSerializer):
-    price = serializers.FloatField(read_only=True)  # Read-only price field
-
-    class Meta:
-        model = Product
-        fields = '__all__'  # Includes all model fields
--- shopping_backend/settings.py
+++ shopping_backend/settings.py
@@ -40,11 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'users',
     'corsheaders',
-    'products',
-    'orders',
-    'email_sender'
 ]
 
 
@@ -132,7 +128,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'users.User'
+# AUTH_USER_MODEL = 'users.User'
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIAL = True
